4fd351ca710426f736502a5c0f6b8c230afc4a8f
Alejandro Robles <alejandro_r1704@hotmail.com>
2020-07-09 20:39:53 -0500

    Location Sharing project created

---
+++ Location Sharing/Location Sharing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Location Sharing
+//
+//  Created by Omar Alejandro Robles Altamirano on 09/07/20.
+//  Copyright © 2020 Omar Alejandro Robles Altamirano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
