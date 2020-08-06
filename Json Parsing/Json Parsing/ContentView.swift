a8b6666690900afe786f1215f0a3ad6695c2be6d
AlejandroRoblesA <alejandro_r1704@hotmail.com>
2020-08-06 11:18:54 -0500

    Json Parsing Project

---
+++ Json Parsing/Json Parsing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Json Parsing
+//
+//  Created by Omar Alejandro Robles Altamirano on 06/08/20.
+//  Copyright © 2020 Omar Alejandro Robles Altamirano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
