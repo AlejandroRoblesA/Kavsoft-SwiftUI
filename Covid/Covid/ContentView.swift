640aed3c9faf2f242b5868b3d2062e5654da4756
AlejandroRoblesA <alejandro_r1704@hotmail.com>
2020-08-23 18:20:31 -0500

    Covid UI

--- Covid/Covid/ContentView.swift
+++ Covid/Covid/ContentView.swift
@@ -23,7 +23,7 @@ struct Home: View{
     var body: some View{
         
         VStack{
-            HStack{
+            HStack(alignment: .top){
                 VStack(alignment: .leading, spacing: 15){
                     Text("Date")
                         .fontWeight(.semibold)
@@ -36,6 +36,9 @@ struct Home: View{
                         .font(.title)
                         .foregroundColor(.white)
                 }
+                
+                Spacer()
+                
                 Button(action:{
                     
                 }){
@@ -47,13 +50,116 @@ struct Home: View{
             .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)! + 18)
             .padding()
             .padding(.bottom, 80)
+            .background(Color.red)
             
-            Spacer()
+            HStack(spacing: 15){
+                
+                VStack(alignment: .leading, spacing: 15){
+                    Text("Deaths")
+                        .foregroundColor(Color.black.opacity(0.5))
+                    Text("3667")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+                .padding(30)
+                .background(
+                    Color.white
+                        .cornerRadius(12)
+                )
+                
+                VStack(alignment: .leading, spacing: 15){
+                    Text("Recovered")
+                        .foregroundColor(Color.black.opacity(0.5))
+                    Text("3667")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 30)
+                .background(
+                    Color.white
+                        .cornerRadius(12)
+                )
+            }
+            .offset(y: -60)
+            .padding(.bottom, -60)
+            .zIndex(25)
+            
+            VStack(alignment: .center, spacing: 15){
+                Text("Active cases")
+                    .foregroundColor(Color.black.opacity(0.5))
+                Text("3667")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.yellow)
+            }
+            
+            .padding(30)
+            .background(
+                Color.white
+                    .cornerRadius(12)
+            )
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack(spacing: 15){
+                    ForEach(1...15, id:\.self){ i in
+                        CellView()
+                    }
+                }
+                .padding()
+            }
         }
         .edgesIgnoringSafeArea(.top)
         .background(
             Color.black.opacity(0.2)
                 .edgesIgnoringSafeArea(.all)
         )
+    }
+}
+
+struct CellView: View{
+    var body: some View{
+        VStack(alignment: .leading, spacing: 15){
+            Text("USA")
+                .fontWeight(.bold)
+            HStack(spacing: 22){
+                VStack(alignment: .leading, spacing: 12){
+                    Text("Active Cases")
+                        .font(.title)
+                    Text("222,111")
+                        .font(.title)
+                }
+                
+                VStack(alignment: .leading, spacing: 12){
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Deaths")
+                        
+                        Text("123")
+                            .foregroundColor(.red)
+                    }
+                    Divider()
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Recovered")
+                        
+                        Text("123")
+                            .foregroundColor(.green)
+                    }
+                    Divider()
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Critial")
+                        
+                        Text("123")
+                            .foregroundColor(.yellow)
+                    }
+                    
+                }
+            }
+        }
+        .padding()
+        .frame(width: UIScreen.main.bounds.width - 30)
+        .background(Color.white)
+        .cornerRadius(20)
     }
 }
